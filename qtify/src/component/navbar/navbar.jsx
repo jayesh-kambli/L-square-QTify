8af764aa6a8c5636b81ac98d08709333287f81a3
Jayesh <Jayesh@gmail.com>
2024-05-15 02:44:49 +0530

    poppin fixed

--- qtify/src/component/navbar/navbar.jsx
+++ qtify/src/component/navbar/navbar.jsx
@@ -1,60 +1,54 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Button from '@mui/material/Button';
+import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
 import "./navbar.style.css";
 import logo from "./logo.png";
 import Stack from "@mui/material/Stack";
 import SearchBar from "../searchBar/searchBar";
+import styledd from 'styled-components';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      'Poppins',
-      'Arial',
-      'sans-serif',
-    ].join(','),
-  },
-});
+const StyledButton = styledd.button`
+  font-family: 'Poppins', sans-serif;
+  /* Add more styles as needed */
+`;
 
 function Nav() {
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static" className="bg">
-          <Toolbar width="100%">
-            <Stack
-              direction="row"
-              justifyContent="space-between"
-              alignItems="center"
-              className="maxWidth"
-            >
-              <Stack direction="row" justifyContent="center" alignItems="center">
-                <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="open drawer"
-                  sx={{ mr: 2 }}
-                >
-                  <img
-                    src={logo}
-                    alt="Logo"
-                    style={{ width: "80px", height: "100%" }}
-                  />
-                </IconButton>
-              </Stack>
-              <SearchBar />
-              <Stack>
-                <Button variant="contained" className="button">Give Feedback</Button>
-              </Stack>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" className="bg">
+        <Toolbar width="100%">
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            className="maxWidth"
+          >
+            <Stack direction="row" justifyContent="center" alignItems="center">
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+              >
+                <img
+                  src={logo}
+                  alt="Logo"
+                  style={{ width: "80px", height: "100%" }}
+                />
+              </IconButton>
             </Stack>
-          </Toolbar>
-        </AppBar>
-      </Box>
-    </ThemeProvider>
+            <SearchBar />
+            <Stack>
+              <StyledButton className="button">Give Feedback</StyledButton>
+            </Stack>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
