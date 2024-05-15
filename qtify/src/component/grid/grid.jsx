641b39d99f233bc770e6a84e468136c9993c1e8b
Jayesh <Jayesh@gmail.com>
2024-05-15 22:15:23 +0530

    final before stream (grid)

--- qtify/src/component/grid/grid.jsx
+++ qtify/src/component/grid/grid.jsx
@@ -6,6 +6,7 @@ import MainList from "./mainList/mainList";
 
 export default function Grid() {
     const [data, dataFn] = useState([]);
+    const [data2, dataFn2] = useState([]);
     const [layout, layoutFn] = useState();
 
   useEffect(() => {
@@ -14,6 +15,15 @@ export default function Grid() {
         .get("https://qtify-backend-labs.crio.do/albums/top")
         .then((response) => {
             dataFn(response.data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+
+        axios
+        .get("https://qtify-backend-labs.crio.do/albums/new")
+        .then((response) => {
+            dataFn2(response.data);
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -27,7 +37,7 @@ export default function Grid() {
   return (
     <>
     <MainList data={[...data]} key={"dsf"}/>
-    <MainList data={[...data]} key={"fdw"}/>
+    <MainList data={[...data2]} key={"fdw"}/>
     </>
   );
 }
